ce975e436a056b2eda58c8bd95057d4bf04f2cdc
xxgingersnapsxx <zxgingersnapsxz@gmail.com>
2021-06-18 15:36:07 +0900

    initial commit

--- src/jdbcTest/JdbcTest01.java
+++ src/jdbcTest/JdbcTest01.java
@@ -1,10 +1,15 @@
 package jdbcTest;
 
 public class JdbcTest01 {
-
+/*
+ 	JDBC(Java DataBase Connectivity)라이브러리를 이용하여 DB자료 처리하기
+ 	
+ 	- 데이터베이스 처리 순서
+ 		1. 드라이버 로딩 ==> 라이브러리를 사용할 수 있게 메모리로 읽어들이는 작업
+ 			Class.forname("oracle.jdbc.driver.OracleDriver");
+ */	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		initial commit;
 	}
 
 }
